895f48c05d9f31e1061a90a86bdc5db51516093f
ADMPHG <65092815+ADMPHG@users.noreply.github.com>
2024-06-13 17:07:39 +0100

    Added the ability to select difficulty. Added all 6 strings with notes up the the 17th fret.

--- practice.py
+++ practice.py
@@ -4,30 +4,63 @@
 import random
 import time
 
-# TODO Goal 1 - given a sequence of notes, display them one at a time in order
-# TODO Goal 2 - adjustable difficulty level that changes the speed at which the next note is displayed
-# TODO Goal 3 - ?
+# COMPLETE Goal 1 - given a string, display notes from the string randomly one at a time
+# COMPLETE Goal 2 - adjustable difficulty level that changes the string selection and speed at which the next note is displayed
+# TODO Goal 3 - gui
+# TODO Goal 4 - custom difficulty settings
+
+### How it works
+# - user is prompted to enter a difficulty setting from 1-3
+# - the higher the number, the more strings are used to produce the random sequence of notes
+# - currently, the time until the next note is displayed is determined by the difficulty setting
 
 # Data Structures
-fretboard = [['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A']]
+fretboard = [['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E (12th fret)', 'F (>12th fret)', 'F# (>12th fret)', 'G (>12th fret)', 'G# (>12th fret)', 'A (>12th fret)'],
+             ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A (12th fret)', 'A# (>12th fret)', 'B (>12th fret)', 'C (>12th fret)', 'C# (>12th fret)', 'D (>12th fret)'],
+             ['D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D (12th fret)', 'D# (>12th fret)', 'E (>12th fret)', 'F (>12th fret)', 'F# (>12th fret)', 'G (>12th fret)'],
+             ['G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G (12th fret)', 'G# (>12th fret)', 'A (>12th fret)', 'A# (>12th fret)', 'B (>12th fret)', 'C (>12th fret)'],
+             ['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B (12th fret)', 'C (>12th fret)', 'C# (>12th fret)', 'D (>12th fret)', 'D# (>12th fret)', 'E (>12th fret)'],
+             ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E (12th fret)', 'F (>12th fret)', 'F# (>12th fret)', 'G (>12th fret)', 'G# (>12th fret)', 'A (>12th fret)']
+             ]
+stringCodes = ['E', 'A', 'D', 'G', 'B', 'E (High)']
 
 class App():
+    def selectDifficulty(self, difficulty):
+        strings = seqLength = pauseTime = 0
+        match difficulty:
+            case '1':
+                strings = [0, 1]
+                seqLength = 10
+                pauseTime = 5
+            case '2':
+                strings = [0, 1, 2, 3]
+                seqLength = 20
+                pauseTime = 4
+            case '3':
+                strings = [0, 1, 2, 3, 4, 5]
+                seqLength = 30
+                pauseTime = 3
+            case _:
+                print('Please enter a valid difficulty setting.')
+        
+        return strings, seqLength, pauseTime
     
-    def genSequence(self, strings):
+    def genSequence(self, strings, seqLength):
         sequence = []
-        while len(sequence) < 9:
+        while len(sequence) < seqLength:
             string = random.choice(strings)
             note = fretboard[string][random.randrange(len(fretboard[string]))]
-            sequence.append(note)
+            sequence.append([string, note])
         return sequence
 
     def run(self):
-        strings = [int(input("Enter strings to practice: "))]
+        difficulty = input("Select Difficulty (Easy - 1, Medium - 2, Hard - 3): ")
+        strings, seqLength, pauseTime = self.selectDifficulty(difficulty)
         print(strings)
-        sequence = self.genSequence(strings)
+        sequence = self.genSequence(strings, seqLength)
         for note in sequence:
-            print(note)
-            time.sleep(3)
+            print(f'{stringCodes[note[0]]} string, {note[1]}')
+            time.sleep(pauseTime)
         print('Sequence complete.')
 
 app = App()
